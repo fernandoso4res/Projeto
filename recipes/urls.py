6cdd390f2d577a9d487c525899bf526bad1ee834
Fernando Soares <fernando.so4res2001@icloud.com>
2022-11-16 01:09:59 -0300

    aplicacao com sql

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from . import views
 
-from recipes.views import home
+app_name = 'recipes'
 
 urlpatterns = [
-    path('', home),  # Home
+    path('', views.home, name="home"),
+
+    path('recipes/<int:id>/', views.recipe, name="recipe"),
 ]
